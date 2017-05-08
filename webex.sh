81ff714e3ff82793859acc0f1a77a0f0d11cba49
Natale Vinto <ebballon@gmail.com>
2017-05-08 10:40:10 +0200

    Add xauth

--- webex.sh
+++ webex.sh
@@ -1,11 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 docker rm --force webex >/dev/null 2>&1
 docker build --tag=webex .
-docker run -ti \
-	--env DISPLAY=unix$DISPLAY \
-	--privileged \
-        --name=webex \
-	--volume /dev/snd:/dev/snd \
-	--volume /tmp/.X11-unix:/tmp/.X11-unix \
-	webex $1
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run -it \
+        --volume=$XSOCK:$XSOCK:rw \
+        --volume=$XAUTH:$XAUTH:rw \
+        --env="XAUTHORITY=${XAUTH}" \
+        --env="DISPLAY=:0" \
+        --user="webex" \
+        webex
