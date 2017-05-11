d5dd5d129013303c26cad3f7b9b364f7cb414371
Natale Vinto <ebballon@gmail.com>
2017-05-12 01:06:56 +0200

    Adding Desktop entries for GNOME/KDE to run the bash script

---
+++ run-webex.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run -it \
+        --volume=$XSOCK:$XSOCK:rw \
+        --volume=$XAUTH:$XAUTH:rw \
+        --env="XAUTHORITY=${XAUTH}" \
+        --env="DISPLAY=$DISPLAY" \
+        --user="webex" \
+        webex
