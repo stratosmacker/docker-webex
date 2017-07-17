3924bae5491a8db0020f5f2f64201b078e9034b5
Garrett Hyde <ghyde@redhat.com>
2017-07-17 12:39:12 -0700

    Create ~/.Xauthority if missing.

--- run-webex.sh
+++ run-webex.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 touch $XAUTH
+
+# Ensure xauth has been created for :0
+if ! xauth list | grep -q "$(hostname --short)/unix:0"; then
+    touch ~/.Xauthority
+    xauth add ${DISPLAY} . $(mcookie)
+fi
+
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+xhost local:root
 
 docker run -it \
         --volume=$XSOCK:$XSOCK:rw \
