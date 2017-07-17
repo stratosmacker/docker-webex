eaf3e4934b8465b2824a3edbad1bdf0ea30f7f16
Garrett Hyde <ghyde@redhat.com>
2017-07-17 12:34:55 -0700

    Remove code duplication.
    `webex.sh` calls `run-webex.sh`.

--- webex.sh
+++ webex.sh
@@ -3,15 +3,4 @@
 docker rm --force webex >/dev/null 2>&1
 docker build --tag=webex .
 
-XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
-touch $XAUTH
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-
-docker run -it \
-        --volume=$XSOCK:$XSOCK:rw \
-        --volume=$XAUTH:$XAUTH:rw \
-        --env="XAUTHORITY=${XAUTH}" \
-        --env="DISPLAY=$DISPLAY" \
-        --user="webex" \
-        webex
+./run-webex.sh
